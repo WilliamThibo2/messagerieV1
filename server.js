c26a13f4104ba11da93bdc64a517ae336ea90145
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-03 10:26:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const session = require('express-session');
-const sharedSession = require('socket.io-express-session');
 const http = require('http');
 const socketIo = require('socket.io');
 const connectDB = require('./config/db');
@@ -17,12 +16,15 @@ const io = socketIo(server, {
     },
 });
 
+// Connexion à la base de données
 connectDB();
 
+// Configuration des middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
+// Configuration du middleware de session
 const sessionMiddleware = session({
     secret: 'votreSecretSession', // Changez pour un secret plus sûr
     resave: false,
@@ -30,26 +32,32 @@ const sessionMiddleware = session({
     cookie: { secure: false } // Mettez `secure: true` si vous utilisez HTTPS
 });
 
-// Utilisez le middleware de session avec Express et Socket.io
+// Utiliser la session avec Express
 app.use(sessionMiddleware);
-io.use(sharedSession(sessionMiddleware, { autoSave: true }));
 
+// Partager la session entre Express et Socket.io
+io.use((socket, next) => {
+    sessionMiddleware(socket.request, socket.request.res || {}, next);
+});
+
+// Routes
 app.use('/api/auth', authRoutes);
 
+// Gérer les utilisateurs connectés
 let connectedUsers = {};
 
 io.on('connection', (socket) => {
     console.log('Nouvelle connexion:', socket.id);
 
     socket.on('join', ({ email }) => {
-        socket.handshake.session.userEmail = email;
-        socket.handshake.session.save();
+        socket.request.session.userEmail = email;
+        socket.request.session.save();
         connectedUsers[email] = socket.id;
         console.log(`${email} connecté avec l'ID ${socket.id}`);
     });
 
     socket.on('private_message', ({ to, message }) => {
-        const userEmail = socket.handshake.session.userEmail;
+        const userEmail = socket.request.session.userEmail;
         if (userEmail) {
             const recipientSocket = connectedUsers[to];
             if (recipientSocket) {
@@ -71,6 +79,7 @@ io.on('connection', (socket) => {
     });
 });
 
+// Démarrer le serveur
 server.listen(process.env.PORT, () => {
     console.log(`Serveur démarré sur le port ${process.env.PORT}`);
 });
