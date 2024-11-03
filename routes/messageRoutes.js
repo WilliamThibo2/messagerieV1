b8fa1ed97de8b48da756c1556d15899961186df8
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-03 18:07:17 -0500

    Update messageRoutes.js

--- routes/messageRoutes.js
+++ routes/messageRoutes.js
@@ -1,14 +1,9 @@
-// routes/messageRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const messageController = require('../controllers/messageController');
 const authController = require('../controllers/authController');
 
-// Route pour envoyer un message privé
 router.post('/send', authController.verifyToken, messageController.sendMessage);
-
-// Route pour récupérer l'historique des messages avec un autre utilisateur
 router.get('/:toEmail', authController.verifyToken, messageController.getMessages);
 
 module.exports = router;
