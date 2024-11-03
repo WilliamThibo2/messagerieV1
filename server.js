bb86c8c991e76da1e5886446333fe4b3ac8b98fc
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-03 10:11:29 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('express-session');
+const sharedSession = require('socket.io-express-session');
 const http = require('http');
 const socketIo = require('socket.io');
 const connectDB = require('./config/db');
@@ -22,32 +23,33 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
-// Configuration de la session
-app.use(session({
+const sessionMiddleware = session({
     secret: 'votreSecretSession', // Changez pour un secret plus sûr
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false } // Mettez `secure: true` si vous utilisez HTTPS
-}));
+});
+
+// Utilisez le middleware de session avec Express et Socket.io
+app.use(sessionMiddleware);
+io.use(sharedSession(sessionMiddleware, { autoSave: true }));
 
 app.use('/api/auth', authRoutes);
 
-// Déclaration des utilisateurs connectés en session
 let connectedUsers = {};
 
-// Gestion des connexions socket.io
 io.on('connection', (socket) => {
     console.log('Nouvelle connexion:', socket.id);
 
     socket.on('join', ({ email }) => {
-        // Associer l'e-mail à l'ID de session
-        socket.request.session.userEmail = email;
+        socket.handshake.session.userEmail = email;
+        socket.handshake.session.save();
         connectedUsers[email] = socket.id;
         console.log(`${email} connecté avec l'ID ${socket.id}`);
     });
 
     socket.on('private_message', ({ to, message }) => {
-        const userEmail = socket.request.session.userEmail; // Utiliser l'e-mail stocké en session
+        const userEmail = socket.handshake.session.userEmail;
         if (userEmail) {
             const recipientSocket = connectedUsers[to];
             if (recipientSocket) {
