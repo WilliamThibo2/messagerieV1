650b83272e354c26faff459b17b2568fe4730530
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-02 21:36:51 -0400

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,29 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.register = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ email, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: 'Utilisateur créé' });
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de l\'inscription' });
+    }
+};
+
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ error: 'Identifiants incorrects' });
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la connexion' });
+    }
+};
