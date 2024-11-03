676169caa2dcd545c5372c0da82f8b3443cb9b0f
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-03 18:05:38 -0500

    Update messageController.js

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,21 +1,17 @@
-// controllers/messageController.js
-
 const Message = require('../models/Message');
 const User = require('../models/User');
 
 // Envoyer un message privé
 exports.sendMessage = async (req, res) => {
     const { to, content } = req.body;
-    const from = req.userId; // Utilisateur authentifié
+    const from = req.userId;
 
     try {
-        // Trouver le destinataire par email
         const recipient = await User.findOne({ email: to });
         if (!recipient) {
             return res.status(404).json({ error: "Destinataire introuvable" });
         }
 
-        // Créer et enregistrer le message
         const message = new Message({ from, to: recipient._id, content });
         await message.save();
 
@@ -26,25 +22,23 @@ exports.sendMessage = async (req, res) => {
     }
 };
 
-// Récupérer l'historique des messages privés entre deux utilisateurs
+// Récupérer l'historique des messages privés
 exports.getMessages = async (req, res) => {
     const { userId } = req;
     const { toEmail } = req.params;
 
     try {
-        // Trouver l'utilisateur correspondant à l'email destinataire
         const recipient = await User.findOne({ email: toEmail });
         if (!recipient) {
             return res.status(404).json({ error: "Destinataire introuvable" });
         }
 
-        // Récupérer les messages échangés entre les deux utilisateurs
         const messages = await Message.find({
             $or: [
                 { from: userId, to: recipient._id },
                 { from: recipient._id, to: userId }
             ]
-        }).sort({ timestamp: 1 }); // Tri par date
+        }).sort({ timestamp: 1 });
 
         res.json(messages);
     } catch (error) {
@@ -52,3 +46,4 @@ exports.getMessages = async (req, res) => {
         res.status(500).json({ error: "Erreur lors de la récupération des messages" });
     }
 };
+
