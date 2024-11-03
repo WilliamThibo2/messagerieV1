4203913ff9851e3e44a6900e7114b4ec8b056581
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-03 18:06:10 -0500

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,9 +1,8 @@
-// models/Message.js
 const mongoose = require('mongoose');
 
 const MessageSchema = new mongoose.Schema({
-    from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Référence à l'expéditeur
-    to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },   // Référence au destinataire
+    from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     content: { type: String, required: true },
     timestamp: { type: Date, default: Date.now }
 });
