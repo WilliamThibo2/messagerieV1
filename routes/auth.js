fbfd37c038d4b2a8eeed478628857a281064b351
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-04 17:01:32 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+const { register, login, deleteAccount } = require('../controllers/authController');
+const { verifyToken } = require('../controllers/authController'); // Assurez-vous que la vérification du token est importée
 
 router.post('/register', register); 
-router.post('/login', login);       
+router.post('/login', login);
+router.delete('/delete', verifyToken, deleteAccount); // Route de suppression avec vérification du token
 
 module.exports = router;
