76bc7e1cb64ede4adb4a0e78221d10f3f189f6d2
WilliamThibo2 <williamthibault@hotmail.fr>
2024-11-03 20:44:01 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
+const verifyToken = require('../middleware/authMiddleware'); // Middleware pour vérifier le token
 const { register, login } = require('../controllers/authController');
 
 router.post('/register', register); 
 router.post('/login', login);       
+
+// Route d'inscription
+router.post('/register', authController.register);
+
+// Route de connexion
+router.post('/login', authController.login);
+
+// Route protégée pour vérifier le token
+router.get('/verifyToken', verifyToken, authController.verifyToken);
+
+// Nouvelle route pour supprimer le compte
+router.delete('/deleteAccount', verifyToken, authController.deleteAccount);
+
+module.exports = router;
 
 module.exports = router;
